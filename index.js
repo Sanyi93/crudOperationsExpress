f8532bfe070e72a2b5245fa2c2222f2495bfe188
TMS <tms@tmsuhuhehehe.hu>
2025-05-18 16:23:38 -0400

    users file modified

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const routes = require('./routes/users.js');
 const app = express();
 const PORT = 5000;
 
+//POST - creating a resource
+//PUT - updating a resource
+
 // Use JSON parsing middleware and user routes
-app.use(express.json());
-app.use("/user", routes);
+app.use(express.json()); // handling requests as a json object
+app.use("/user", routes); // handling endpoint starting with "/user" - those are defined in "routes" folder in the file "users.js"
 
 // Start the server and log a message when it's running
 app.listen(PORT, () => console.log("Server is running at port " + PORT));
--- routes/users.js
+++ routes/users.js
@@ -25,24 +25,35 @@ let users = [
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    res.send(uers)//retrieve the array of objects defined above
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    //extracting the email parameter from the url request
+    const email = req.params.email;
+    //filtering users based on the email in the request body
+    filtered_users = users.filter((user) => user.email === email);
+    //sending filtered_users as the response to the client
+    res.send(filtered_users)
 });
 
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    //pushing a new user to the users array
+    users.push({
+        "firstName:": req.query.firstName,
+        "lastName": req.query.lastName,
+        "email": req.query.email,
+        "DOB": req.query.DOB
+    });
+  // Sending a message indicating which user has been recently added
+  res.send("The user "+ req.query.firstName + " has been added!");
 });
-
-
+///////////////////////////////////////////////////////////////////////////////////////////////
+//STEP No 5 - CONTINUE///
+//////////////////////////////////////////////////////////////////////////////////////////////
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
   // Copy the code here
